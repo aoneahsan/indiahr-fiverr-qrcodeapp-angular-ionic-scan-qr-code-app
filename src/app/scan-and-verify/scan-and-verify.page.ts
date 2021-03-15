842de2a3694bd8431bbae6eb476947f42ee5e1d5
ahsanmahmood786 <ahsan.mahmood@shopdev.co>
2021-03-16 00:02:34 +0500

    fix: updated

--- src/app/scan-and-verify/scan-and-verify.page.ts
+++ src/app/scan-and-verify/scan-and-verify.page.ts
@@ -7,7 +7,7 @@ import { LoadingController } from '@ionic/angular';
   templateUrl: './scan-and-verify.page.html',
   styleUrls: ['./scan-and-verify.page.scss'],
 })
-export class ScanAndVerifyPage {
+export class ScanAndVerifyPage implements OnInit {
   qrResultString;
   validQRs = [
     'Sproxil Authentication',
@@ -17,23 +17,39 @@ export class ScanAndVerifyPage {
     'Pac-Man',
     'Super Mario World',
   ];
+  processing: boolean = false;
   constructor(
     private _router: Router,
     private _loadingCtl: LoadingController
   ) {}
+
+  ngOnInit() {
+    this.processing = false;
+  }
+
   onCodeResult(resultString: string) {
+    this.processing = true;
     this.qrResultString = resultString;
     const qrIndex = this.validQRs.findIndex((el) => el == resultString);
     this._loadingCtl.create({ message: 'Processing...' }).then((loadingEl) => {
       loadingEl.present();
       setTimeout(() => {
         loadingEl.dismiss();
-        if (qrIndex > -1) {
-          this._router.navigate(['/verification-success']);
-        } else {
-          this._router.navigate(['/verification-failed']);
-        }
       }, 1600);
+      setTimeout(() => {
+        this.loadingDone(qrIndex);
+      }, 2200);
     });
+  }
+
+  loadingDone(qrIndex) {
+    if (qrIndex > -1) {
+      this._router.navigate(['/verification-success']);
+    } else {
+      this._router.navigate(['/verification-failed']);
+    }
+    setTimeout(() => {
+      this.processing = false;
+    }, 1000);
   }
 }
